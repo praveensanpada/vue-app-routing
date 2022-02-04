c12d632e769cde75ac7a37f61e5033fefdf7ceb8
praveen_sanpada <praveen.sanpada@aurigait.com>
2022-02-04 13:51:22 +0530

    pp

---
+++ vue-routing-app/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> |
+    <router-link to="/contact">About</router-link> |
+    <router-link to="/login">About</router-link> |
+    <router-link to="/register">About</router-link>
+  </div>
+ 
+  <router-view />
+</template>
+
